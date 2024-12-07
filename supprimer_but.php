342816e0da0fd5d49f4dd7fef75cb42ef02cf315
Asma Bej <107648335+duzcoder@users.noreply.github.com>
2024-12-07 11:28:28 +0100

    Update supprimer_but.php

--- supprimer_but.php
+++ supprimer_but.php
@@ -14,10 +14,10 @@ if (isset($_POST['idm']) && is_array($_POST['idm'])) {
     $requete->execute($idmArray);
 
     // Redirection après la suppression
-    header('Location: tp6affichage.php');
+    header('Location: index.php');
     exit();
 } else {
-    header('Location: tp6affichage.php?message=Aucun but selectionne');
+    header('Location: index.php?message=Aucun but selectionne');
     exit();
 }
 ?>
